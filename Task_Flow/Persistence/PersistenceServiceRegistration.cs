46419f24435e4f3e7787def59d6c27511057d122
Long6703 <nguyenkhaclong6703@gmail.com>
2025-02-17 07:43:04 +0700

    init project

---
+++ Task_Flow/Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
